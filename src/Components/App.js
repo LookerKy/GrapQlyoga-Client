16b9bcd2454a309ead11a2f7544e44b8b0b6728e
LookerKy <nagaraz07@gmail.com>
2019-02-16 14:53:46 +0900

    add router

--- src/Components/App.js
+++ src/Components/App.js
@@ -1,12 +1,20 @@
 import React, { Component } from "react";
 import GlobalStyles from "Components/GlobalStyles";
+import Home from "Components/Home";
+import Detail from "Components/Detail";
+import { HashRouter as Router, Route } from "react-router-dom";
 import { ApolloProvider } from "react-apollo";
 import client from "../apolloClient";
 class App extends Component {
   render() {
     return (
       <ApolloProvider client={client}>
-        <GlobalStyles />
+        <Router>
+          <React.Fragment>
+            <Route path="/" exact component={Home} />
+            <Route path="/detail/:movieId" component={Detail} />
+          </React.Fragment>
+        </Router>
       </ApolloProvider>
     );
   }
